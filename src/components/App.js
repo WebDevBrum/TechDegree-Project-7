02ea99cbff5c9a03e304cadfba9f8141d8b27d7a
Scott Davies <scottdavies1983@gmail.com>
2020-08-01 22:56:00 +0100

    end of session

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import React from 'react';
 import Search from './Search';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
+import apiKey from './config';
+
+const api = apiKey;
+console.log(api); //delete this it works
 
 
 function App() {
